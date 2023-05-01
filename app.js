dc11152fa94023386dcdbf9825dcdc6dceb65f9f
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-01 18:42:30 +0000

    f

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ const currentImage = new Image();
 currentImage.src = ''; // Set the default image source if needed
 
 function drawText() {
-    const topText = topTextInput.value;
-    const bottomText = bottomTextInput.value;
-  
-    ctx.font = '48px Impact';
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.textAlign = 'center';
-    ctx.fillText(topText, canvas.width / 2, 50);
-    ctx.strokeText(topText, canvas.width / 2, 50);
-    ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-    ctx.strokeText(bottomText, canvas.width / 2, canvas.height - 20);
-  }
-  
+  const topText = topTextInput.value;
+  const bottomText = bottomTextInput.value;
+
+  ctx.font = '48px Impact';
+  ctx.fillStyle = 'white';
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.textAlign = 'center';
+  ctx.fillText(topText, canvas.width / 2, 50);
+  ctx.strokeText(topText, canvas.width / 2, 50);
+  ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
+  ctx.strokeText(bottomText, canvas.width / 2, canvas.height - 20);
+}
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(currentImage, 0, 0, canvas.width, canvas.height);
+  drawText();
+}
 
 function loadImage(src) {
   currentImage.src = src;
-  currentImage.onload = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(currentImage, 0, 0, canvas.width, canvas.height);
-  };
+  currentImage.onload = updateCanvas;
 }
 
 document.querySelectorAll('.img-thumbnail').forEach(img => {
@@ -38,19 +40,10 @@ document.querySelectorAll('.img-thumbnail').forEach(img => {
   });
 });
 
-topTextInput.addEventListener('focus', function() {
-  this.value = '';
-});
-
-bottomTextInput.addEventListener('focus', function() {
-  this.value = '';
-});
+topTextInput.addEventListener('input', updateCanvas);
+bottomTextInput.addEventListener('input', updateCanvas);
 
 generateMemeBtn.addEventListener('click', function() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(currentImage, 0, 0, canvas.width, canvas.height);
-  drawText();
-
   // Save the image
   const link = document.createElement('a');
   link.href = canvas.toDataURL('image/jpeg');
@@ -58,28 +51,9 @@ generateMemeBtn.addEventListener('click', function() {
   link.click();
 });
 
-postOnTwitterBtn.addEventListener('click', async function() {
-  // Save the image as a data URL
-  const dataURL = canvas.toDataURL('image/jpeg');
-
-  // Convert the data URL to a Blob
-  const response = await fetch(dataURL);
-  const blob = await response.blob();
-
-  // Create an instance of the IPFS HTTP client
-  const ipfs = window.ipfsHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
-
-
-  // Add the image to IPFS
-  const { path } = await ipfs.add(blob);
-
-  // Construct the public IPFS URL
-  const publicURL = `https://ipfs.io/ipfs/${path}`;
-
-  // Construct the tweet
-  const tweetText = encodeURIComponent('Check out my meme! @meetnippy');
-  const tweetURL = `https://twitter.com/intent/tweet?url=${publicURL}&text=${tweetText}`;
-
-  // Open the tweet dialog in a new window
-  window.open(tweetURL, '_blank');
+postOnTwitterBtn.addEventListener('click', function() {
+  const url = canvas.toDataURL('image/png');
+  const encodedUrl = encodeURIComponent(url);
+  const twitterUrl = `https://twitter.com/intent/tweet?url=${encodedUrl}&text=Check%20out%20my%20meme!&hashtags=meme&via=meetnippy`;
+  window.open(twitterUrl, '_blank');
 });
