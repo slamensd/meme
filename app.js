1f22adcb5d20923f8bba0ee6b70ebbd7a8b57a7e
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-01 18:23:02 +0000

    v

--- app.js
+++ app.js
@@ -40,6 +40,33 @@ $(document).ready(function() {
         });
     });
 
+    // Replace the previous event listener with this one
+postOnTwitterBtn.addEventListener('click', async function() {
+    // Save the image as a data URL
+    const dataURL = canvas.toDataURL('image/jpeg');
+  
+    // Convert the data URL to a Blob
+    const response = await fetch(dataURL);
+    const blob = await response.blob();
+  
+    // Create an instance of the IPFS HTTP client
+    const ipfs = window.IpfsHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+  
+    // Add the image to IPFS
+    const { path } = await ipfs.add(blob);
+  
+    // Construct the public IPFS URL
+    const publicURL = `https://ipfs.io/ipfs/${path}`;
+  
+    // Construct the tweet
+    const tweetText = encodeURIComponent('Check out my meme! @meetnippy');
+    const tweetURL = `https://twitter.com/intent/tweet?url=${publicURL}&text=${tweetText}`;
+  
+    // Open the tweet dialog in a new window
+    window.open(tweetURL, '_blank');
+  });
+  
+
     topTextInput.addEventListener('focus', function() {
         this.value = '';
       });
